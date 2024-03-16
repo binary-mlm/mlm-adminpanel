c8c717269ea864a2cef6057cb7c8e0edab4aa04a
srijanibanerje <srijaniofficial2022@gmail.com>
2024-03-16 14:42:52 +0530

    "data not bind"

--- src/views/Addproduct.js
+++ src/views/Addproduct.js
@@ -1,19 +1,50 @@
-import React from 'react'
-
+/* eslint-disable prettier/prettier */
+import { useState, React } from 'react'
 import {
     CCardFooter,
     CCard,
-
     CFormInput,
     CCol,
     CRow,
     CForm,
     CFormLabel,
-    CButton,
-
+    CButton
 } from '@coreui/react'
-
+import axios from 'axios';
+import swal from 'sweetalert';
 const Addproduct = () => {
+    const [courseid, setCourseid] = useState('')
+    const [course_name, setCoursename] = useState('')
+    const [course_review, setCoursereview] = useState('')
+    const [videos, setVideos] = useState('')
+    const [teacher_name, setTeacher_name] = useState('')
+    const [teacher_dept, setTeacher_dept] = useState('')
+    
+    //handle submit
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        // alert("submit works")
+        if (courseid === "" || course_name === "" || course_review === "" || teacher_name === "" || teacher_dept === "" || videos === "") {
+            swal("Opps!", "Please fill out all required fields!", "error");
+        }
+
+        else {
+
+            axios.post('http://localhost:8000/api/v1/product/new', { courseid, course_name, course_review, videos, teacher_name, teacher_dept  })
+                .then(res => {
+                    console.log(res);
+                    swal("yeah", "Product sucessfully inserted!", "success");
+                })
+                .catch(err => {
+                    console.log(err);
+                    swal("Opps!", "Not inserted !", "error");
+
+                })
+
+        }
+
+    }
+
     return (
         <>
             <CRow>
@@ -28,6 +59,7 @@ const Addproduct = () => {
                                     id="exampleFormControlInput1"
                                     name="courseid"
                                     placeholder="Enter course id"
+                                    onChange={e => setCourseid(e.target.value)}
                                 />
                             </div>
                             <div className="mb-3">
@@ -36,6 +68,7 @@ const Addproduct = () => {
                                     id="exampleFormControlInput2"
                                     name='course_name'
                                     placeholder="Enter course name"
+                                    onChange={e => setCoursename(e.target.value)}
                                 />
 
                             </div>
@@ -47,37 +80,42 @@ const Addproduct = () => {
                                     name='course_review'
                                     id="exampleFormControlInput3"
                                     placeholder="Enter course review"
+                                    onChange={e => setCoursereview(e.target.value)}
                                 />
                             </div>
                             <div className="mb-3">
-                                <CFormLabel htmlFor="exampleFormControlInput4">Teacher's name</CFormLabel>
+                                <CFormLabel htmlFor="exampleFormControlInput6">Total Video</CFormLabel>
+                                <CFormInput
+                                    type="text"
+                                    name='videos'
+                                    id="exampleFormControlInput6"
+                                    placeholder="Enter total video"
+                                    onChange={e => setVideos(e.target.value)}
+                                />
+                            </div>
+                            <div className="mb-3">
+                                <CFormLabel htmlFor="exampleFormControlInput4">Teachers name</CFormLabel>
                                 <CFormInput
                                     type="text"
                                     name='teacher_name'
                                     id="exampleFormControlInput4"
-                                    placeholder="Enter course id"
+                                    placeholder="Enter Teacher name"
+                                    onChange={e => setTeacher_name(e.target.value)}
                                 />
                             </div>
                             <div className="mb-3">
-                                <CFormLabel htmlFor="exampleFormControlInput5">Teacher's dept</CFormLabel>
+                                <CFormLabel htmlFor="exampleFormControlInput5">Teachers dept</CFormLabel>
                                 <CFormInput
                                     type="text"
                                     name='teacher_dept'
                                     id="exampleFormControlInput5"
-                                    placeholder="Enter course id"
+                                    placeholder="Enter Teacher's dept"
+                                    onChange={e => setTeacher_dept(e.target.value)}
                                 />
                             </div>
-                            <div className="mb-3">
-                                <CFormLabel htmlFor="exampleFormControlInput5">Total Video</CFormLabel>
-                                <CFormInput
-                                    type="number"
-                                    name='videos'
-                                    id="exampleFormControlInput5"
-                                    placeholder="Enter course id"
-                                />
-                            </div>
+                           
                             <div className='text-center'>
-                            <CButton  as="input"className='btn w-25' type="submit" color="primary" value="Submit" />
+                                <CButton as="input" className='btn w-25' type="submit" color="primary" value="Submit"  onClick={handleSubmit} />
                             </div>
                         </CForm>
 
--- src/views/dashboard/Dashboard.js
+++ src/views/dashboard/Dashboard.js
@@ -1,4 +1,6 @@
-import React from 'react'
+/* eslint-disable prettier/prettier */
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 import {
   CCardFooter,
@@ -21,6 +23,20 @@ import WidgetsDropdown from '../widgets/WidgetsDropdown'
 // import MainChart from './MainChart'
 
 const Dashboard = () => {
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    fetchData();
+}, []);
+const fetchData = async () => {
+  try {
+      const response = await axios.get('http://localhost:8000/api/v1/product');
+      setData(response.data);
+  } catch (error) {
+      console.error('Error fetching data:', error);
+  }
+};
+
   return (
     <>
       <WidgetsDropdown className="mb-4" />
@@ -32,24 +48,41 @@ const Dashboard = () => {
           <CTable color="dark">
             <CTableHead>
               <CTableRow>
-                <CTableHeaderCell scope="col">Class</CTableHeaderCell>
-                <CTableHeaderCell scope="col">Heading</CTableHeaderCell>
-                <CTableHeaderCell scope="col">Heading</CTableHeaderCell>
+                <CTableHeaderCell scope="col">Course_id</CTableHeaderCell>
+                <CTableHeaderCell scope="col">Course name</CTableHeaderCell>
+                <CTableHeaderCell scope="col">Review</CTableHeaderCell>
+                <CTableHeaderCell scope="col">video</CTableHeaderCell>
+                <CTableHeaderCell scope="col">Teacher name</CTableHeaderCell>
+                <CTableHeaderCell scope="col">Teacher dept</CTableHeaderCell>
               </CTableRow>
             </CTableHead>
             <CTableBody>
-              <CTableRow active>
+             
+            {data.map((row, index) => (
+                    <CTableRow active key={index}> 
+                    <CTableDataCell scope="row">{row.course_id}</CTableDataCell>
+                    <CTableDataCell>{row.course_name}</CTableDataCell>
+                    <CTableDataCell>{row.course_review}</CTableDataCell>
+                    <CTableDataCell>{row.total_video}</CTableDataCell>
+                    <CTableDataCell>{row.teacher_name}</CTableDataCell>
+                    
+                    <CTableDataCell>{row.teacher_dept}</CTableDataCell>
+                   
+                    </CTableRow>
+                   
+                 ))} 
+              {/* <CTableRow active>
                 <CTableHeaderCell scope="row">1</CTableHeaderCell>
                 <CTableDataCell>Mark</CTableDataCell>
                 <CTableDataCell>Otto</CTableDataCell>
                 <CTableDataCell>@mdo</CTableDataCell>
-              </CTableRow>
+             
               <CTableRow>
                 <CTableHeaderCell scope="row">2</CTableHeaderCell>
                 <CTableDataCell>Jacob</CTableDataCell>
                 <CTableDataCell>Thornton</CTableDataCell>
                 <CTableDataCell>@fat</CTableDataCell>
-              </CTableRow>
+              </CTableRow> */}
             </CTableBody>
           </CTable>
         </CCardBody>
