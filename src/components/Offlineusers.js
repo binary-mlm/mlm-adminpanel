11137cd9961f3762c58f885c7049a21593b0c008
srijanibanerje <srijaniofficial2022@gmail.com>
2024-07-09 19:03:43 +0530

    "latest 09-07"

--- src/components/Offlineusers.js
+++ src/components/Offlineusers.js
@@ -12,13 +12,19 @@ import {
   import axios from 'axios';
   const Offlineusers = () => {
     const [student , setStudent] = useState([]);
+    const [payment,setpayment] = useState([]);
     useEffect(() =>{
         axios.get('http://localhost:3000/api/auth/getofflineuser')
         .then(student => setStudent(student.data.data))
         .catch(err => console.log(err))
        
     }, []);
-    
+  //   useEffect(() =>{
+  //     axios.get('http://localhost:3000/api/auth/getpayment')
+  //     .then(payment => setpayment(payment.data.data))
+  //     .catch(err => console.log(err))
+     
+  // }, []);
     return (
         <>
         <div className='fw-bold'>
@@ -26,10 +32,14 @@ import {
         <CTable responsive="sm" color="dark">
         <CTableHead align="middle">
           <CTableRow  >
-            <CTableHeaderCell scope="col" className='col-3' >Student name</CTableHeaderCell>
-            <CTableHeaderCell scope="col" className='col-3'>Course name </CTableHeaderCell>
+          <CTableHeaderCell scope="col" className='col-2'>order_id</CTableHeaderCell>
+            <CTableHeaderCell scope="col" className='col-2' >Student name</CTableHeaderCell>
+            <CTableHeaderCell scope="col" className='col-2'>Course name </CTableHeaderCell>
             <CTableHeaderCell scope="col" className='col-2'>Student phno</CTableHeaderCell>
-            <CTableHeaderCell scope="col" className='col-3'>Transaction photo</CTableHeaderCell>
+            <CTableHeaderCell scope="col" className='col-1'>Amount</CTableHeaderCell>
+            <CTableHeaderCell scope="col" className='col-2'>Payment_id</CTableHeaderCell>
+            
+          
             <CTableHeaderCell scope="col" className='col-1'>Action</CTableHeaderCell>
           </CTableRow>
         </CTableHead>
@@ -37,11 +47,13 @@ import {
         {
               student.map((user,index) => {
                 return  <CTableRow active key={index}>
+                <CTableHeaderCell scope="col" className='col-2'>order_id</CTableHeaderCell>
         
-                <CTableDataCell className='col-3'>{user.fullname}</CTableDataCell>
-                <CTableDataCell className='col-3'>{user.course}</CTableDataCell>
+                <CTableDataCell className='col-2'>{user.fullname}</CTableDataCell>
+                <CTableDataCell className='col-2'>{user.course}</CTableDataCell>
                 <CTableDataCell className='col-2'>{user.phoneno}</CTableDataCell>
-                        <CTableDataCell className='col-3'><img width={150}  height={150} src={user.image} /></CTableDataCell>
+                        <CTableDataCell className='col-1'>{user.amount}</CTableDataCell>
+                        <CTableHeaderCell scope="col" className='col-2'>Payment_id</CTableHeaderCell>
                         <CTableDataCell>
                           <a><i className="fa fa-edit ms-2 editicon"></i></a> 
                          </CTableDataCell>
